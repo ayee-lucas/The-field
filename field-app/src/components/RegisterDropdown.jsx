f626913f9dc526685d910f5a02b38635e5f42174
Alan Lopez <alopez-2018459@kinal.edu.gt>
2023-03-24 10:44:40 -0600

    bug fixes

--- field-app/src/components/RegisterDropdown.jsx
+++ field-app/src/components/RegisterDropdown.jsx
@@ -22,7 +22,7 @@ const RegisterDropdown = ({ sports }) => {
     <>
       <div
         onClick={handleDropDown}
-        className="relative flex justify-between px-3 transition-all cursor-pointer items-center w-full my-5 rounded-md bg-green-700 hover:border hover:border-white select-none"
+        className="relative flex justify-between px-3 transition-all cursor-pointer items-center w-full my-5 rounded-md bg-green-700 hover:border hover:border-black select-none"
       >
         <h1 className="">Select your sport</h1>
         <BsArrowDownShort
@@ -35,7 +35,7 @@ const RegisterDropdown = ({ sports }) => {
               className={
                 dropDown
                   ? "hidden"
-                  : "visible absolute rounded-sm top-16 left-0 right-0 py-3 bg-green-700 z-50 text-center hover:border border-white"
+                  : "visible absolute rounded-sm top-16 left-0 right-0 py-3 bg-green-700 z-50 text-center hover:border border-black border"
               }
             >
              {sport.sport} 
--- field-app/src/components/RegisterForm.jsx
+++ field-app/src/components/RegisterForm.jsx
@@ -6,7 +6,7 @@ const RegisterForm = () => {
   return (
     <form
       action=""
-      className="bg-black/70 px-4 backdrop-blur-2xl w-full h-full mt-[5rem] max-w-[1000px] max-h-[640px]  min-w-[400px]"
+      className="bg-white px-4 backdrop-blur-2xl w-full h-full mt-[5rem] max-w-[1000px] max-h-[640px]  min-w-[400px]"
     >
       <div className="flex relative login-bg w-full h-[200px] bg-center bg-cover">
         <div className="absolute bg-black/70 w-full top-0 left-0 right-0 bottom-0  z-[2]" />
@@ -21,7 +21,7 @@ const RegisterForm = () => {
          */}
         <input
           type="text"
-          class=" placeholder:text-slate-400 my-3 py-3 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+          class=" placeholder:text-green-700 my-3 py-3 cursor-pointer  block text-center font-bold bg-white  w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
           placeholder="Email"
         />
       </label>
@@ -31,7 +31,7 @@ const RegisterForm = () => {
            */}
           <input
             type="text"
-            class=" placeholder:text-slate-400 my-5 cursor-pointer py-3  block text-center text-white bg-black w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+            class=" placeholder:text-green-700 my-5 cursor-pointer py-3  block text-center  bg-white w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
             placeholder="Username"
           />
         </label>
@@ -44,7 +44,7 @@ const RegisterForm = () => {
            */}
           <input
             type="text"
-            class=" placeholder:text-slate-400 my-5 py-3 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+            class=" placeholder:text-green-700 my-5 py-3 cursor-pointer  block text-center  bg-state w-full border border-green-600 rounded-md  pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
             placeholder="Password"
           />
         </label>
@@ -54,7 +54,7 @@ const RegisterForm = () => {
            */}
           <input
             type="password"
-            class=" placeholder:text-slate-400 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+            class=" placeholder:text-green-700 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
             placeholder="Confirm Password"
           />
         </label>
@@ -66,7 +66,7 @@ const RegisterForm = () => {
            */}
           <input
             type="text"
-            class=" placeholder:text-slate-400 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+            class=" placeholder:text-green-700 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
             placeholder="Name"
           />
         </label>
@@ -76,7 +76,7 @@ const RegisterForm = () => {
            */}
           <input
             type="text"
-            class=" placeholder:text-slate-400 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
+            class=" placeholder:text-green-700 my-5 cursor-pointer  block text-center text-white bg-black w-full border border-green-600 rounded-md py-2 pr-3 shadow-sm focus:outline-none focus:ring-black  focus:ring-1 sm:text-sm"
             placeholder="Last Name"
           />
         </label>
